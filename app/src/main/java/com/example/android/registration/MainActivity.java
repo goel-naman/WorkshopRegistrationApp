a142d773c7b2408fcc374539d5b20c9271e77fcd
Khushal98 <41577754+Khushal98@users.noreply.github.com>
2018-09-24 21:34:26 +0530

    created a login fragment

--- app/src/main/java/com/example/android/registration/MainActivity.java
+++ app/src/main/java/com/example/android/registration/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.registration;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addFragment();
     }
+
+    private void addFragment() {
+        Fragment fragment;
+        fragment = new LoginFragment();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.fragments,fragment,"NoFragment").addToBackStack(null)
+                .commit();
+    }
+
+
 }
